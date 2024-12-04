211684e6791f8c630fe38b810dba6fe6a61688fd
Vjviki <148333939+Vjviki@users.noreply.github.com>
2024-12-04 22:00:26 +0530

    Update index.js

--- speedometer-class-function/myapp/src/components/speedometer/index.js
+++ speedometer-class-function/myapp/src/components/speedometer/index.js
@@ -1,56 +1,58 @@
 import {Component} from 'react'
+
 import './index.css'
 
 class Speedometer extends Component {
-  state = {count: 0}
-
-  accelerate = () => {
-    this.setState(prevState => {
-      if (prevState.count === 200) {
-        return {count: 200}
-      }
-      return {count: prevState.count + 10}
-    })
+  state = {
+    speed: 0,
   }
 
-  applyBrake = () => {
-    this.setState(prevState => {
-      if (prevState.count === 0) {
-        return {count: 0}
-      }
-      return {count: prevState.count - 10}
-    })
+  onClickApplyBrakeButton = () => {
+    const {speed} = this.state
+
+    if (speed > 0) {
+      this.setState(prevState => ({speed: prevState.speed - 10}))
+    }
+  }
+
+  onClickAccelerateButton = () => {
+    const {speed} = this.state
+
+    if (speed < 200) {
+      this.setState(prevState => ({speed: prevState.speed + 10}))
+    }
   }
 
   render() {
-    const {count} = this.state
+    const {speed} = this.state
+
     return (
-      <div className="speedo-container">
-        <h1 className="heading">SPEEDOMETER</h1>
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png"
-          alt="speedometer"
-          className="img-size"
-        />
-        <h1 className="display-meter">Speed is {count}mph</h1>
-        <p className="limit-description">
-          Min Limit is 0mph, Max Limit is 200mph
-        </p>
-        <div>
-          <button
-            type="button"
-            className="accelerate-button"
-            onClick={this.accelerate}
-          >
-            Accelerate
-          </button>
-          <button
-            type="button"
-            className="brake-button"
-            onClick={this.applyBrake}
-          >
-            Apply Brake
-          </button>
+      <div className="speedometer-app-container">
+        <div className="speedometer-container ">
+          <h1 className="heading">SPEEDOMETER</h1>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png"
+            alt="speedometer"
+            className="speedometer-image"
+          />
+          <h1 className="speed-text">Speed is {speed}mph</h1>
+          <p className="speed-limits">Min Limit is 0mph, Max Limit is 200mph</p>
+          <div className="buttons-container">
+            <button
+              type="button"
+              className="accelerate-button button"
+              onClick={this.onClickAccelerateButton}
+            >
+              Accelerate
+            </button>
+            <button
+              type="button"
+              className="apply-brake-button button"
+              onClick={this.onClickApplyBrakeButton}
+            >
+              Apply Brake
+            </button>
+          </div>
         </div>
       </div>
     )
